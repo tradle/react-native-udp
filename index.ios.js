c17555b4ea65623b1a5c9d4ebcbbefe1fba5ed51
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-05-10 10:59:44 -0400

    fix example

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
@@ -70,14 +66,7 @@ var rctsockets = React.createClass({
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          Open Dev Tools to see socket chatter
         </Text>
       </View>
     );
