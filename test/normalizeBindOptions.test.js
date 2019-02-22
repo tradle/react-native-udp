02c31de453249de6c276c11bfe3a2ff67e3df042
Mark Vayngrib <mvayngrib@gmail.com>
2019-02-22 08:58:55 -0500

    test: alt opts to bind()

--- test/normalizeBindOptions.test.js
+++ test/normalizeBindOptions.test.js
@@ -3,13 +3,14 @@ const normalizeBindOptions = require('../normalizeBindOptions')
 const prettify = obj => JSON.stringify(obj, null, 2)
 
 describe('normalizeBindOptions', () => {
-  const args = [
-    { name: 'port', value: 1234 },
-    { name: 'address', value: '1.2.3.4' },
-    { name: 'callback', value: () => {} },
-  ]
 
-  it('should support all combinations of arguments', () => {
+  it('should support all combinations of arguments for [port], [address], [callback]', () => {
+    const args = [
+      { name: 'port', value: 1234 },
+      { name: 'address', value: '1.2.3.4' },
+      { name: 'callback', value: () => {} },
+    ]
+
     // check every combination of arguments (retaining order)
     // use a bit mask to decide whether to keep an argument
     for (let i = 0, numCombinations = Math.pow(2, args.length); i < numCombinations; i++) {
@@ -23,6 +24,31 @@ describe('normalizeBindOptions', () => {
         assert.deepEqual(result, expected)
       } catch (err) {
         throw new Error(`for args: ${prettify(usedArgsValues)}\nexpected: ${prettify(expected)}\ngot ${prettify(result)}`)
+      }
+    }
+  })
+
+  it('should support all combinations of arguments for [options], [callback]', () => {
+    const callback = () => {}
+    const inOut = [
+      [
+        [{ port: 123 }, callback], { port: 123, callback }
+      ],
+      [
+        [{ port: 123 }], { port: 123 }
+      ],
+      [
+        [callback], { callback }
+      ],
+    ]
+
+    for (const [args, expected] of inOut) {
+      let result
+      try {
+        result = normalizeBindOptions(...args)
+        assert.deepEqual(result, expected)
+      } catch (err) {
+        throw new Error(`for args: ${prettify(args)}\nexpected: ${prettify(expected)}\ngot ${prettify(result)}`)
       }
     }
   })
